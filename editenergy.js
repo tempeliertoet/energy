4f95394886c879687e741eb9e648b08c14d0e0a6
tempeliertoet <tempeliertoet@example.com>
2018-11-19 19:55:09 +0100

    converted editenergy to vue app

--- editenergy.js
+++ editenergy.js
@@ -1,60 +1,61 @@
-function startApp(){
-    var apiKey = getApiKey();
-    if(!apiKey){
-        apiKey = window.prompt("ApiKey");
-        localStorage.setItem('apikey', apiKey);
-    }
-    
-    start(apiKey);
-}
-
-function getApiKey(){
-    return localStorage.getItem('apikey');
-}
-
-function getAuthentication(){
-    return {
-        auth: {
-            username: 'tempeliertoet',
-            password: getApiKey()
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        fileContent: null,
+        apiKey: null,
+        sha: null
+    },
+    computed: {
+        solarYieldChartData: function(){
+            return this.getChartData('solarYield');
         }
-    };
-}
+    },
+    methods: {
+        getApiKey(){
+            return localStorage.getItem('apikey');
+        },
+        getAuthentication(){
+            return {
+                auth: {
+                    username: 'tempeliertoet',
+                    password: this.getApiKey()
+                }
+            };
+        },
+        async getFileInfo() {
+            try {
+                const response = await axios.get('https://api.github.com/repos/tempeliertoet/energy/contents/energy.json', this.getAuthentication());
+                return response.data;
+            }
+            catch (error) {
+                console.error(error);
+            }
+        },
+        async updateFile() {
+            try {
+                var data = {
+                    message: 'testcommit',
+                    content: btoa(this.fileContent),
+                    sha: this.sha
+                };
+                const response = await axios.put('https://api.github.com/repos/tempeliertoet/energy/contents/energy.json', data, this.getAuthentication());
+                this.sha = response.data.content.sha;
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+    },
+    async created() {
+        var apiKey = this.getApiKey();
+        if(!apiKey){
+            apiKey = window.prompt("ApiKey");
+            localStorage.setItem('apikey', apiKey);
+        }
+        this.apiKey = apiKey;
 
-async function start(){
-    var fileInfo = await getFileInfo();
-    var filecontentElement = document.getElementById('filecontent');
-    filecontentElement.value = atob(fileInfo.content);
-    var shaElement = document.getElementById('filesha');
-    shaElement.value = fileInfo.sha;
-}
-
-async function getFileInfo() {
-    try {
-        const response = await axios.get('https://api.github.com/repos/tempeliertoet/energy/contents/energy.json', getAuthentication());
-        var data = response.data;
-        console.log(data);
-        return data;
+        var fileInfo = await this.getFileInfo();
+        this.fileContent = atob(fileInfo.content);
+        this.sha = fileInfo.sha;
     }
-    catch (error) {
-        console.error(error);
-    }
-}
-
-async function updateFile() {
-    var content = document.getElementById('filecontent').value;
-    var sha = document.getElementById('filesha').value;
-    try {
-        var data = {
-            message: 'testcommit',
-            content: btoa(content),
-            sha: sha
-        };
-        await axios.put('https://api.github.com/repos/tempeliertoet/energy/contents/energy.json', data, getAuthentication());
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
-
-startApp();
+});
